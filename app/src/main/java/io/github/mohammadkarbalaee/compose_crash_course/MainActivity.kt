51b7f13ca0cba659f0d00c486bb556090e83e766
Mohammad karbalaee <muhammad.ksht@gmail.com>
2023-07-16 10:12:38 +0200

    Learnt the basics of jetpack compose

--- app/src/main/java/io/github/mohammadkarbalaee/compose_crash_course/MainActivity.kt
+++ app/src/main/java/io/github/mohammadkarbalaee/compose_crash_course/MainActivity.kt
@@ -3,44 +3,68 @@ package io.github.mohammadkarbalaee.compose_crash_course
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.sp
 import io.github.mohammadkarbalaee.compose_crash_course.ui.theme.Compose_crash_courseTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalMaterial3Api
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Compose_crash_courseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                var name by remember {
+                    mutableStateOf("")
+                }
+                var names by remember {
+                    mutableStateOf(listOf<String>())
+                }
+
+                Column(
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Greeting("Android")
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        OutlinedTextField(value = name, onValueChange = { text ->
+                            name = text
+                        })
+
+                        Button(onClick = {
+                            names = names + name
+                            name = ""
+                        }) {
+                            Text(text = "Add")
+                        }
+                    }
+
+                    LazyColumn {
+                        items(names) { currentName ->
+                            Text(text = currentName, fontSize = 40.sp)
+                            Divider()
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Compose_crash_courseTheme {
-        Greeting("Android")
-    }
-}
